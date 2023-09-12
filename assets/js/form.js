682a094bb3a5c1ee17aba137f05268355407129c
Felipe Carneiro <correaflp@gmail.com>
2023-09-11 22:58:59 -0300

    Feat: 1.50
    [NewFeat] - New logic to send email with specific domain
    
    - Building new text for webpage

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,1 +1,24 @@
-// enviar um formulário via ajax
+var enviar = document.querySelector('#submit');
+var validDomainList = ['gmail.com', 'hotmail.com', 'hotmail.com.br', 'outlook.com.br']
+
+function atualizarEmail() {    
+    var newEmail = document.querySelector('#email').value;
+    return newEmail
+}
+
+function isValidDomain(email) {
+    var userDomain = email.split('@')[1];
+    if (validDomainList.includes(userDomain)){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+enviar.onclick = function (event) {
+    var email = atualizarEmail();
+    if(!isValidDomain(email)) {
+        alert('Please, enter a valid email: Gmail, Hotmail or Outlook with ".com" or ".com.br"');
+        event.preventDefault();
+    }
+}
